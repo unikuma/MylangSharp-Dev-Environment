6256d79fb9eccb2f8831255c807c12a9ed0065e1
unikuma <85827428+unikuma@users.noreply.github.com>
2023-02-02 15:06:41 +0900

    プロジェクト追加

---
+++ MylangSharp/MylangSharp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Livet;
+using Livet.Commands;
+using Livet.EventListeners;
+using Livet.Messaging;
+using Livet.Messaging.IO;
+using Livet.Messaging.Windows;
+using MylangSharp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MylangSharp.ViewModels
+{
+	public class MainWindowViewModel : ViewModel
+	{
+		// Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+		public void Initialize()
+		{
+		}
+	}
+}
---
+++ MylangSharp/MylangSharp/Views/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MylangSharp.Views
+{
+	/* 
+     * If some events were receive from ViewModel, then please use PropertyChangedWeakEventListener and CollectionChangedWeakEventListener.
+     * If you want to subscribe custome events, then you can use LivetWeakEventListener.
+     * When window closing and any timing, Dispose method of LivetCompositeDisposable is useful to release subscribing events.
+     *
+     * Those events are managed using WeakEventListener, so it is not occurred memory leak, but you should release explicitly.
+     */
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
